2932c9e2e42ee8999b1b8b4ad4bbf26a7ae7f7be
Artem0904 <moonportal708@gmail.com>
2024-04-05 17:02:07 +0300

    implement routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PizzasListComponent } from './pizzas/pizzas-list/pizzas-list.component';
+import { AddPizzaComponent } from './pizzas/add-pizza/add-pizza.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent },
+    { path: "pizzas", component: PizzasListComponent },
+    { path: "add", component: AddPizzaComponent }
+];
